c067d1cbd8fc91e8ae04d2c97f7317d6fc1aba1c
Duncan Bain <duncan@xps>
2022-07-17 22:21:31 +0100

    setup.sh

---
+++ setup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+export DEBIAN_FRONTEND=noninteractive
+export INSTALL_ZSH=true
+export USERNAME=`whoami`
+
+sudo apt-get update
+sudo apt-get -y install --no-install-recommends apt-utils dialog 2>&1
+sudo apt-get install -y \
+  curl \
+  git \
+  sudo \
+  openssh-client \
+  less \
+  wget \
+  unzip \
+  apt-transport-https \
+  lsb-release 
+
+# Install & Configure Zsh
+if [ "$INSTALL_ZSH" = "true" ]
+then
+    sudo apt-get install -y \
+    fonts-powerline \
+    zsh
+
+    chsh -s /usr/bin/zsh $USERNAME
+    wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+fi
+
+sudo ln -fs /usr/share/zoneinfo/Europe/London /etc/localtime
+sudo dpkg-reconfigure --frontend noninteractive tzdata
